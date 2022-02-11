1f7c391820b8a9d1bba6155798639f5da2ca0eb3
Mrugendra Shilvant <sanketshilvant@gmail.com>
2022-02-11 15:02:14 +0530

    ..

--- src/components/page4/Page4.jsx
+++ src/components/page4/Page4.jsx
@@ -4,7 +4,7 @@ import image from '../../assets/Group 2503.png'
 export default function () {
     return (
         <div className='container mx-auto w-11/12'>
-            <div style={{ backgroundColor: "#FFE3B2", boxShadow: "0px 12px 20px 10px rgba(0, 0, 0, 0.07)" }} className="mt-10 px-12 py-8 flex justify-between items-start mx-auto rounded-xl">
+            <div style={{ backgroundColor: "#FFE3B2", boxShadow: "0px 12px 20px 10px rgba(0, 0, 0, 0.07)" }} className="mt-16 px-12 py-8 flex justify-between items-start mx-auto rounded-xl">
                 <div className="left">
                     <h3 className="text-2xl font-semibold">Already have a Resume?</h3>
                     <p className="mt-2">Upload your Resume, and instanly change designs</p>
